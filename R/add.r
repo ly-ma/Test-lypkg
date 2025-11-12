1f6fd3ef526ada7505d1e9392165d31f0ec34b80
ly-ma <malanyue@126.com>
2025-11-12 14:03:37 +0000

    A

---
+++ R/add.r
@@ -0,0 +1,16 @@
+#' @title Add samples
+#' @describeIn Add x and y.
+#'
+#' @param x one number
+#' @param y one number
+#'
+#' @return
+#' @export
+#' @importFrom base sum
+#' @examples add(1,2)
+#' \donttest{
+#' add(1,2)
+#' }
+add <- function(x,y) {
+  x+y
+}
